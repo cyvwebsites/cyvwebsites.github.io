34d9ad39cdaac97eca73555c5c9ac6d495fbad27
Cristian Ventura <99362764+cristian-ventura@users.noreply.github.com>
2023-11-16 14:35:18 -0600

    Added project to index and new tab
    - added new button
    - added new project tab

--- JS/project_four_script.js
+++ JS/project_four_script.js
@@ -5,39 +5,17 @@ var browser_height = window.innerHeight;
 var screen_width = screen.width;
 var screen_height = screen.height;
 
-//function called to visit linked profile when left side pic/logo is clicked
-function go_to_linkein()
+//function called to go back to home page
+function go_home()
 {
     'use strict';
-    window.location.href = 'https://www.linkedin.com/in/cristian-ventura-43531a168/';
+    window.location.href = 'index.html';
 }
-//function called when top button is called, which will go to the designated page
-// to show the projects
-function button_one()
+// function called to go to project website
+function go_to_project()
 {
     'use strict';
-    window.location.href = 'project_one.html';
-}
-//function called when middle button is called, which will go to the designated page
-// to show the projects
-function button_two()
-{
-    'use strict';
-    window.location.href = 'project_two.html';
-}
-//function called when bottom button is called, which will go to the designated page
-// to show the projects
-function button_three()
-{
-    'use strict';
-    window.location.href = 'project_three.html';
-}
-//function called when bottom button is called, which will go to the designated page
-// to show the projects
-function button_four()
-{
-    'use strict';
-    window.location.href = 'project_four.html';
+    window.location.href = 'https://miventuracounseling.com/';
 }
 
 //Function used to make website work with whatever screen/browser width is used
@@ -50,15 +28,15 @@ function checkScreen()
     'use strict';
     if((screen_width <= 1000) || (browser_width <= 1000))
     {
-        document.getElementById("left_side_intro").style.width = "100%";
-        document.getElementById("right_side_intro").style.width = "100%";                                  
+        document.getElementById("left_side_container").style.width = "100%";
+        document.getElementById("right_side_container").style.width = "100%";                                  
     }
     //If screen or browser is greater then 1000
     //then change website back to regular view
     if((screen_width > 1000) && (browser_width > 1000))
     {
-        document.getElementById("left_side_intro").style.width = "50%";
-        document.getElementById("right_side_intro").style.width = "50%";
+        document.getElementById("left_side_container").style.width = "50%";
+        document.getElementById("right_side_container").style.width = "50%";
                           
                            
     }
