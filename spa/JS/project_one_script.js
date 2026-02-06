7a88637e57cb7d70be65090d61a10a9ee6b4bd69
Cristian Ventura <99362764+cristian-ventura@users.noreply.github.com>
2026-02-06 11:57:08 -0600

    Test for "../"
    Testing for when you try to go back to index file inside folder

---
+++ spa/JS/project_one_script.js
@@ -0,0 +1,19 @@
+//Width and height of browser
+var browser_width = window.innerWidth;
+var browser_height = window.innerHeight;
+//Width and height of screen
+var screen_width = screen.width;
+var screen_height = screen.height;
+
+//function called to go back to home page
+function go_home()
+{
+    'use strict';
+    window.location.href = '../';
+}
+// function called to go to project website
+function go_to_project()
+{
+    'use strict';
+   
+}
